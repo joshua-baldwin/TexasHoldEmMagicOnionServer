7a10d7f9436907a560895b8cec093e20a2ae00d3
Joshua Baldwin <joshua_baldwin@rudel.jp>
2024-11-08 11:54:48 +0900

    use kestrel; remove unneeded profiles; update to use cors grpcwebsockets etc

--- TexasHoldEmServer/Program.cs
+++ TexasHoldEmServer/Program.cs
@@ -5,11 +5,48 @@ using TexasHoldEmServer.ServerManager;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();       // Add this line(Grpc.AspNetCore)
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        // WARN: Do not apply following policies to your production.
+        //       If not configured carefully, it may cause security problems.
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+        policy.AllowAnyHeader();
+
+        // NOTE: "grpc-status" and "grpc-message" headers are required by gRPC. so, we need expose these headers to the client.
+        policy.WithExposedHeaders("grpc-status", "grpc-message");
+    });
+});
 builder.Services.AddMagicOnion(); // Add this line(MagicOnion.Server)
 builder.Services.AddSingleton<IServerManager>(new ServerManager());
 
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles(new StaticFileOptions()
+{
+    ServeUnknownFileTypes = true,
+    DefaultContentType = "application/octet-stream",
+    OnPrepareResponse = (ctx) =>
+    {
+        if (ctx.File.Name.EndsWith(".br"))
+        {
+            ctx.Context.Response.Headers.ContentEncoding = "br";
+        }
+        if (ctx.File.Name.Contains(".wasm")) ctx.Context.Response.Headers.ContentType = "application/wasm";
+        if (ctx.File.Name.Contains(".js")) ctx.Context.Response.Headers.ContentType = "application/javascript";
+    }
+});
+app.UseCors();
+app.UseWebSockets();
+app.UseGrpcWebSocketRequestRoutingEnabler();
+
+app.UseRouting();
+
+// NOTE: `UseGrpcWebSocketBridge` must be called after calling `UseRouting`.
+app.UseGrpcWebSocketBridge();
 app.MapMagicOnionService(); // Add this line
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
