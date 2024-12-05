75763e0642cf213b65cf6c291031054bd4edfd6b
Joshua Baldwin <joshua_baldwin@rudel.jp>
2024-12-06 04:25:40 +0900

    change chips to 100

--- TexasHoldEmShared/Utilities/Constants.cs
+++ TexasHoldEmShared/Utilities/Constants.cs
@@ -4,7 +4,7 @@ namespace THE.MagicOnion.Shared.Utilities
     {
         public static int MaxPlayers = 10;
         public static int MaxHoleCards = 2;
-        public static int StartingChips = 50;
+        public static int StartingChips = 100;
         public static int MinBet = 2;
         public static int RaiseAmount = 1;
         public static int JokerCount = 2;
